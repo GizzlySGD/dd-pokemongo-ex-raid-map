5119a66a05bbf0e9902f6ad32785f3feba7620a5
GizzlySGD <35133497+GizzlySGD@users.noreply.github.com>
2018-01-15 10:17:28 +0100

    update gyms

--- compiled.js
+++ compiled.js
@@ -159,7 +159,7 @@ var overlayS2Labels = function overlayS2Labels(s2CellCount) {
   //s2TotalsLayerGroup.addLayer(totals);
 };
 
-fetchLocal("https://cdn.rawgit.com/GizzlySGD/be115bd8f1ae79ae87c6492c5a504860/raw/1d76cd06a30d0b5b1463cab1396789a85da08655/gyms.geojson").then(function (data) {
+fetchLocal("https://cdn.rawgit.com/GizzlySGD/be115bd8f1ae79ae87c6492c5a504860/raw/4cee216f0693f2a62335597b2a7e486dc3e59b11/gyms.geojson").then(function (data) {
   var _ref, _ref2;
 
   gyms = data;
